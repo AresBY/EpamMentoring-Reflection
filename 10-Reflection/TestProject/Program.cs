84c40834c9141df4fa00f876612464a7d9db8c2e
AresBY <free2107@mail.ru>
2021-03-20 02:21:17 +0300

    task

---
+++ 10-Reflection/TestProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using Reflection.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommonTests t = new CommonTests();
+            t.GetPublicObsoleteClasses_Should_Return_Right_List();
+            t.GetProperty_Should_Return_Property_Value_For_Single_Path();
+            t.GetProperty_Should_Return_Property_Value_For_Complex_Path();
+            t.SetProperty_Should_Assign_Value_For_Single_Public_Path();
+            t.GetProperty_Should_Assign_Value_For_Complex_Path();
+            t.SetProperty_Should_Assign_Value_For_Single_Private_Path();
+        }
+    }
+}
